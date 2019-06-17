d65f9acc7e1ea1d8fb3ade6d32a728137d5d875f
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-06-17 22:08:31 +0530

    Navbr w/o highlighter

--- src/app/Index.js
+++ src/app/Index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import Home from "./components/home/Home";
 import * as OfflinePluginRuntime from "offline-plugin/runtime";
+import Navbr from "./components/navbar/NavBar";
 OfflinePluginRuntime.install();
 import "./styles/main.scss";
 class Index extends React.Component {
   render() {
-    return <Home />;
+    return <Navbr />;
   }
 }
 
