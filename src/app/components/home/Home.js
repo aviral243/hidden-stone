6f218f4d5a3114464bde8bc6f055c541e4ee6cf5
qroach <utkarshgill07@gmail.com>
2019-07-01 14:32:49 +0530

    home.js reverted to hello world

--- src/app/components/home/Home.js
+++ src/app/components/home/Home.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import TextField from "../utils/TextField";
-import Button from "../utils/Button";
 class Home extends Component {
   render() {
-    return (
-      <div>
-        <TextField />
-        <Button />
-      </div>
-    );
+    return <div className="main">Hello World!!!</div>;
   }
 }
 
