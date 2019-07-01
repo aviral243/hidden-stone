8285ad8d2c1b8a08931d023a27ce7d8fbd9ce15b
qroach <utkarshgill07@gmail.com>
2019-07-01 11:59:57 +0530

    home js reverted

--- src/app/components/home/Home.js
+++ src/app/components/home/Home.js
@@ -1,18 +1,7 @@
 import React, { Component } from "react";
-import TopBar from "../topbar/TopBar.js";
 class Home extends Component {
   render() {
-    return (
-      <div>
-        <TopBar />
-        <section>
-          <img id="bg1" src={require("../../images/HomeBG.png")} />
-        </section>
-        <section>
-          <img id="bg1" src={require("../../images/HomeBG.png")} />
-        </section>
-      </div>
-    );
+    return <div className="main">Hello World!!!</div>;
   }
 }
 
