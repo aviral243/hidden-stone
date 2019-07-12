24d780d0e6e8abb8fe624d380c17e8915ae8dbea
adrijshikhar <adrijshikhar85@gmail.com>
2019-07-12 11:56:37 +0530

    testin

--- src/app/components/home/Home.js
+++ src/app/components/home/Home.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import { Sections, SubSections } from "./sections/index";
 class Home extends Component {
   render() {
-    return <div className="main">Hello World!!! </div>;
+    return (
+      <div className="main">
+        <Sections.Main />
+        <Sections.OurWork />
+        <Sections.AboutUs />
+        <Sections.Donate />
+        <SubSections.OurEndeavours />
+        <SubSections.OurTeam />
+        <SubSections.OurStory />
+      </div>
+    );
   }
 }
 
