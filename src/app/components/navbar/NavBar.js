395525f6bfb213164e29dbf998a91a11c5bea672
adrijshikhar <adrijshikhar85@gmail.com>
2020-04-06 19:56:51 +0530

    Removes TopBar component

--- src/app/components/navbar/NavBar.js
+++ src/app/components/navbar/NavBar.js
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
-
+import { InstaLogo, FacebookLogo, UFLogo } from "../../images/SVGLogos.js";
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isActive: true,
-      jqueryEaseReady: false,
-      dataScroll: 0,
-      isHovered: false
-    };
-  }
-
   render() {
     return (
       <ul className="NavBarContainer" id="navbar">
+        <div className="uf-logo">
+          <a href={"https://utthanfoundation.co"} id="UFlogo">
+            <UFLogo />
+            <div className="NavBarHeadingContainer">
+              <span className="NavBarHeading">Utthan Foundation</span>
+              <span className="NavBarSubHeading">
+                Your success, our determination
+              </span>
+            </div>
+          </a>
+        </div>
         <li className="NavItem activeLink">
           <a
-            href="#main"
+            href="/#main"
             className="NavItemLinks js-scroll-trigger hover "
             id="nav-main"
           >
@@ -25,7 +26,7 @@ class NavBar extends Component {
         </li>
         <li className="NavItem">
           <a
-            href="#aboutus"
+            href="/#aboutus"
             className="NavItemLinks js-scroll-trigger hover"
             id="nav-main"
           >
@@ -34,7 +35,7 @@ class NavBar extends Component {
         </li>
         <li className="NavItem">
           <a
-            href="#ourwork"
+            href="/#ourwork"
             className="NavItemLinks js-scroll-trigger hover"
             id="nav-main"
           >
@@ -43,33 +44,55 @@ class NavBar extends Component {
         </li>
         <li className="NavItem">
           <a
-            href="#donate"
+            href="/#donate"
             className="NavItemLinks js-scroll-trigger hover"
             id="nav-main"
           >
             JOIN THE JOURNEY
           </a>
         </li>
-        <li className="NavItem activeLink">
+        <li className="NavItem">
           <a
-            href="#ourstory"
+            href="/#ourstory"
             className="NavItemLinks js-scroll-trigger hover "
-            id="nav-sub-section"
+            id="nav-main"
           >
             OUR STORY
           </a>
         </li>
         <li className="NavItem">
           <a
-            href="#ourteam"
+            href="/#ourteam"
             className="NavItemLinks js-scroll-trigger hover"
-            id="nav-sub-section"
+            id="nav-main"
           >
             OUR TEAM
+          </a>
+        </li>
+        <li className="NavItem">
+          <a className="NavItemLinks hover" href={"https://www.google.com"}>
+            CONTACT
+          </a>
+        </li>
+        <li className="NavItem logo">
+          <a
+            href="https://www.facebook.com/utthanfoundation140918/"
+            className="NavItemLinks  hover"
+          >
+            <FacebookLogo />
+          </a>
+        </li>
+        <li className="NavItem logo">
+          <a
+            href="https://www.instagram.com/utthanfoundation/"
+            className="NavItemLinks hover"
+          >
+            <InstaLogo />
           </a>
         </li>
       </ul>
     );
   }
 }
+
 export default NavBar;
