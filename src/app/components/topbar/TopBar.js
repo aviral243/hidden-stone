b0f6fa3602c08dd3a8e06b606ca0eb616566e890
adrijshikhar <adrijshikhar85@gmail.com>
2020-04-06 19:24:40 +0530

    Fixes Topbar component

--- src/app/components/topbar/TopBar.js
+++ src/app/components/topbar/TopBar.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import UFlogo from "../../images/logo.svg";
 import FBlogo from "../../images/FB.svg";
-import Instalogo from "../../images/Insta.svg";
+import { InstaLogo, FacebookLogo } from "../../images/SVGLogos.js";
 class TopBar extends Component {
   render() {
     return (
-      <ul className="TopBarContainer" id="topbar">
-        <div className="web">
-          <div className="logo">
-            <a href={"https://utthanfoundation.co"} id="UFlogo">
-              <img src={UFlogo} />
-              <div className="TopBarHeadingContainer">
-                <span className="TopBarHeading">Utthan Foundation</span>
-                <span className="TopBarSubHeading">
-                  Your success, our determination
-                </span>
-              </div>
-            </a>
-          </div>
+      <ul className="NavBarContainer" id="navbar">
+        <div className="uf-logo">
+          <a href={"https://utthanfoundation.co"} id="UFlogo">
+            <img src={UFlogo} />
+            <div className="NavBarHeadingContainer">
+              <span className="NavBarHeading">Utthan Foundation</span>
+              <span className="TopBarSubHeading">
+                Your success, our determination
+              </span>
+            </div>
+          </a>
         </div>
         <li className="NavItem activeLink">
           <a
@@ -55,11 +53,11 @@ class TopBar extends Component {
             JOIN THE JOURNEY
           </a>
         </li>
-        <li className="NavItem activeLink">
+        <li className="NavItem">
           <a
             href="#ourstory"
             className="NavItemLinks js-scroll-trigger hover "
-            id="nav-sub-section"
+            id="nav-main"
           >
             OUR STORY
           </a>
@@ -68,28 +66,26 @@ class TopBar extends Component {
           <a
             href="#ourteam"
             className="NavItemLinks js-scroll-trigger hover"
-            id="nav-sub-section"
+            id="nav-main"
           >
             OUR TEAM
           </a>
         </li>
-        <div className="contact">
-          <div className="logo">
-            <a href={"https://www.google.com"}>
-              <img src={FBlogo} />
-            </a>
-          </div>
-          <div className="logo">
-            <a href={"https://www.google.com"}>
-              <img src={Instalogo} id="instaLogo" />
-            </a>
-          </div>
-          <div className="logo">
-            <a className="contactUs" href={"https://www.google.com"}>
-              CONTACT
-            </a>
-          </div>
-        </div>
+        <li className="NavItem">
+          <a className="NavItemLinks hover" href={"https://www.google.com"}>
+            CONTACT
+          </a>
+        </li>
+        <li className="NavItem logo">
+          <a href={"https://www.google.com"} className="NavItemLinks  hover">
+            <FacebookLogo />
+          </a>
+        </li>
+        <li className="NavItem logo">
+          <a href={"https://www.google.com"} className="NavItemLinks hover">
+            <InstaLogo />
+          </a>
+        </li>
       </ul>
     );
   }
